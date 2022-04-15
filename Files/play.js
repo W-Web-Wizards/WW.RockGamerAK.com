b9a646c2712ed9910af92748a96d6fc23534ec7c
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2022-04-14 21:12:29 -0700

    Update play.js

--- Files/play.js
+++ Files/play.js
@@ -1,11 +1,11 @@
 function play() {
   var img = document.querySelector('img')
-  img.setAttribute('src', '')
+  img.setAttribute('src', 'https://4u-online.github.io/Files/anim.png')
   img.setAttribute('onclick', 'stop()')
 }
 
 function stop() {
   var img = document.querySelector('img')
-  img.setAttribute('src', '')
+  img.setAttribute('src', 'https://4u-online.github.io/Files/play.png')
   img.setAttribute('onclick', 'play()')
 }
